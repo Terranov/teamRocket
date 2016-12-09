b5089a4037c1ca305a57e85dc639da16b17aa51b
Vinícius Terranova <Terranova@Mac-mini-de-Andrea.local>
2016-12-09 19:26:33 -0200

    tela de detalhe

---
+++ pokemongoloide/PokemonService.swift
@@ -0,0 +1,11 @@
+//
+//  PokemonService.swift
+//  pokemongoloide
+//
+//  Created by Vinícius Terranova on 09/12/16.
+//  Copyright © 2016 Vinícius Terranova. All rights reserved.
+//
+
+import SwiftyJSON
+
+
---
+++ pokemongoloide/Request.swift
@@ -0,0 +1,10 @@
+//
+//  Request.swift
+//  pokemongoloide
+//
+//  Created by Vinícius Terranova on 09/12/16.
+//  Copyright © 2016 Vinícius Terranova. All rights reserved.
+//
+
+import Just
+
